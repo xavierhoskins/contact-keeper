d043764d70abfaf0ce33d291cdb37baa18de3b02
Xavier Hoskins <xavierdhoskins@gmail.com>
2020-10-01 10:41:01 -0500

    added express validator and some checks for users router

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
 
 // @route   POST api/users
 // @desc    Resister a user
 // @access  Public
-router.post('/', (req, res) => {
-  res.send('Register a user');
-});
+router.post(
+  '/',
+  // Check will check the res.body and send an error is it doesnt meet the requirements
+  [
+    check('name', 'Please add name').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with six or more characters'
+    ).isLength({
+      min: 6,
+    }),
+  ],
+  // if there are errors then send an error json
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send('passed');
+  }
+);
 
 module.exports = router;
